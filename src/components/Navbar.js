0a5a8f1cbb570ddd7d5eed2f338c58ae54d72405
Roy Christian Pery <thercpery@gmail.com>
2022-02-22 18:53:18 +0800

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,11 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function AppNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar bg="dark" expand="lg">
+        <Navbar.Brand href="#home">No Brand</Navbar.Brand>
             <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#about">About</Nav.Link>
+                <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
         </Navbar>
     )
